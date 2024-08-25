66caac7e4ea71bbda45a153ca5a080f8dd68d0e0
Abinash Sonar <74831561+abinash0013@users.noreply.github.com>
2024-08-25 16:47:30 +0530

    changes on env file

--- src/constants/Data.constant.js
+++ src/constants/Data.constant.js
@@ -1,34 +1,35 @@
-const mainUrl = `http://localhost:8000/`
-// const mainUrl = `http://178.16.137.208:8000/`
-// const mainUrl = `https://api.sevenstartambola.com/`
-// const mainUrl = `http://api.sevenstartambola.com/`
+// const mainUrl = `http://localhost:8000/`  
+
+const mainUrl = process.env.REACT_APP_API_URL;
+// console.log('API URL:', mainUrl);
+
 const base = {
-  adminLogin: mainUrl + "adminLogin",
-  agentLogin: mainUrl + "agentLogin",
-  saveAgent: mainUrl + "saveAgent",
-  agentsList: mainUrl + "agentsList",
-  agentsOwnDetails: mainUrl + "agentsOwnDetails",
-  editAgent: mainUrl + "editAgent",
-  deleteAgent: mainUrl + "deleteAgent",
-  saveUser: mainUrl + "saveUser",
-  usersList: mainUrl + "usersList",
-  editUser: mainUrl + "editUser",
-  deleteUser: mainUrl + "deleteUser",
-  saveTicket: mainUrl + "saveTicket",
-  ticketList: mainUrl + "ticketList",
-  editTicket: mainUrl + "editTicket",
-  deleteTicket: mainUrl + "deleteTicket",
-  ticketCardView: mainUrl + "ticketCardView",
-  gameList: mainUrl + "gameList",
-  saveGame: mainUrl + "saveGame",
-  editGame: mainUrl + "editGame",
-  viewTicketForAgents: mainUrl + "viewTicketForAgents",
-  bookTicketByAgents: mainUrl + "bookTicketByAgents",
-  agentTransactionHistory: mainUrl + "agentTransactionHistory",
-  agentDebitRequest: mainUrl + "agentDebitRequest",
-  agentBookTicketAmount: mainUrl + "agentBookTicketAmount",
-  agentCreditDebitTransacationList: mainUrl + "agentCreditDebitTransacationList",
-  addMoneyToAgentWallet: mainUrl + "addMoneyToAgentWallet",
-  agentDetails: mainUrl + "agentDetails",
+  adminLogin: `${mainUrl}/adminLogin`,
+  agentLogin: `${mainUrl}/agentLogin`,
+  saveAgent: `${mainUrl}/saveAgent`,
+  agentsList: `${mainUrl}/agentsList`,
+  agentsOwnDetails: `${mainUrl}/agentsOwnDetails`,
+  editAgent: `${mainUrl}/editAgent`,
+  deleteAgent: `${mainUrl}/deleteAgent`,
+  saveUser: `${mainUrl}/saveUser`,
+  usersList: `${mainUrl}/usersList`,
+  editUser: `${mainUrl}/editUser`,
+  deleteUser: `${mainUrl}/deleteUser`,
+  saveTicket: `${mainUrl}/saveTicket`,
+  ticketList: `${mainUrl}/ticketList`,
+  editTicket: `${mainUrl}/editTicket`,
+  deleteTicket: `${mainUrl}/deleteTicket`,
+  ticketCardView: `${mainUrl}/ticketCardView`,
+  gameList: `${mainUrl}/gameList`,
+  saveGame: `${mainUrl}/saveGame`,
+  editGame: `${mainUrl}/editGame`,
+  viewTicketForAgents: `${mainUrl}/viewTicketForAgents`,
+  bookTicketByAgents: `${mainUrl}/bookTicketByAgents`,
+  agentTransactionHistory: `${mainUrl}/agentTransactionHistory`,
+  agentDebitRequest: `${mainUrl}/agentDebitRequest`,
+  agentBookTicketAmount: `${mainUrl}/agentBookTicketAmount`,
+  agentCreditDebitTransacationList: `${mainUrl}/agentCreditDebitTransacationList`,
+  addMoneyToAgentWallet: `${mainUrl}/addMoneyToAgentWallet`,
+  agentDetails: `${mainUrl}/agentDetails`,
 }
 export { base }
